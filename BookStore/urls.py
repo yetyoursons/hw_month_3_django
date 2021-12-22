59a4bca01f4654fce5d198d95bf54d6b53c88521
yetyoursons <aman.b.kurmangaliev@gmail.com>
2021-12-22 23:19:57 +0600

    Add all my files

--- BookStore/urls.py
+++ BookStore/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('posts/', views.get_posts, name='post_view'),
-    path('posts/<int:id>/', views.post_detail, name='post_detail_view'),
-    path('add-post/', views.add_post, name='add_post_view'),
-    path('add-comment/', views.add_comment, name='add_comment_view'),
+    path('posts/', views.PostListView.as_view(), name='post_view'),
+    path("posts/<int:id>/",views.PostDetailView.as_view(),name ='post_detail_view'),
+    path("posts/<int:id>/update/", views.PostUpdateView.as_view(), name='post_update_view'),
+    path("posts/<int:id>/delete/", views.PostDeleteView.as_view(), name='post_delete_view'),
+    path('add-post/', views.PostCreateView.as_view(), name='add_post_view'),
 ]
+
+
--- BookStore/views.py
+++ BookStore/views.py
@@ -1,50 +1,98 @@
 from django.http import Http404, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from . import models, forms
+from django.views import generic
+
+class PostListView(generic.ListView):
+    template_name = 'post_list.html'
+    queryset = models.Post.objects.all()
+
+    def get_queryset(self):
+        return models.Post.objects.all()
+
+class PostDetailView(generic.DetailView):
+    template_name = 'post_detail.html'
+
+    def get_object(self,**kwargs):
+        post_id = self.kwargs.get('id')
+        return get_object_or_404(models.Post, id=post_id)
+
+class PostCreateView(generic.CreateView):
+    template_name = 'add_post.html'
+    form_class = forms.PostForm
+    success_url = '/posts/'
+    queryset = models.Post.objects.all()
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super().form_valid(form=form)
+
+class PostUpdateView(generic.UpdateView):
+    template_name = 'add_post.html'
+    form_class = forms.PostForm
+    success_url = '/posts/'
 
 
-def get_posts(request):
-    post = models.Post.objects.all()
-    return render(request, 'post_list.html', {'post': post})
+    def get_object(self, **kwargs):
+        post_id = self.kwargs.get('id')
+        return get_object_or_404(models.Post, id = post_id)
 
 
-def post_detail(request, id):
-    try:
-        post = models.Post.objects.get(id=id)
-        try:
-            comment= models.Comment.objects.filter(post_id=id).order_by('created_date')
-        except models.Comment.DoesNotExist:
-            return HttpResponse("No Comments")
-    except models.Post.DoesNotExist:
-        raise Http404('Post does not exist')
+    def form_valid(self,form):
+        print(form.cleaned_data)
+        return super().form_valid(form=form)
 
-    return render(request, 'post_detail.html', {'post': post, 'post_comment': comment})
-
-
-def add_post(request):
-    method = request.method
-    if method == "POST":
-        form = forms.PostForm(request.POST, request.FILES)
-        print(form.data)
-        models.Post.objects.create(title=form.data['title'],
-                                   description=form.data['description'])
-        return HttpResponse('Post Created Successfully')
-    else:
-        form = forms.PostForm()
-
-    return render(request, 'add_post.html', {'form': form})
-
-def add_comment(request):
-    method = request.method
-    if method == 'POST':
-        form = forms.CommentForm(request.POST, request.FILES)
-        print(form.data)
-        if form.is_valid():
-            form.save()
-            return  HttpResponse("Comment Created")
-    else:
-        form= forms.CommentForm()
-    return render(request, 'add_comment.html', {'form':form})
+class PostDeleteView(generic.DeleteView):
+    template_name = 'post_delete.html'
+    success_url = '/posts/'
+    def get_object(self,**kwargs):
+        post_id = self.kwargs.get("id")
+        return get_object_or_404(models.Post,id=post_id)
 
 
-
+# def get_posts(request):
+#     post = models.Post.objects.all()
+#     return render(request, 'post_list.html', {'post': post})
+#
+#
+# def post_detail(request, id):
+#     try:
+#         post = models.Post.objects.get(id=id)
+#         try:
+#             comment= models.Comment.objects.filter(post_id=id).order_by('created_date')
+#         except models.Comment.DoesNotExist:
+#             return HttpResponse("No Comments")
+#     except models.Post.DoesNotExist:
+#         raise Http404('Post does not exist')
+#
+#     return render(request, 'post_detail.html', {'post': post, 'post_comment': comment})
+#
+#
+# def add_post(request):
+#     method = request.method
+#     if method == "POST":
+#         form = forms.PostForm(request.POST, request.FILES)
+#         print(form.data)
+#         models.Post.objects.create(title=form.data['title'],
+#                                    description=form.data['description'],
+#                                    image=form.data['image'])
+#         return HttpResponse('Post Created Successfully')
+#     else:
+#         form = forms.PostForm()
+#
+#     return render(request, 'add_post.html', {'form': form})
+#
+# def add_comment(request):
+#     method = request.method
+#     if method == 'POST':
+#         form = forms.CommentForm(request.POST, request.FILES)
+#         print(form.data)
+#         if form.is_valid():
+#             form.save()
+#             return  HttpResponse("Comment Created")
+#     else:
+#         form= forms.CommentForm()
+#     return render(request, 'add_comment.html', {'form':form})
+#
+#
+#
